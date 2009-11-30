a7184496d30089d77306fe92dff1c78d412fb68e
Peter Wagenet <peter.wagenet@gmail.com>
2009-11-30 12:20:36 -0500

    Added labels and Reloaded view

--- apps/rich_text_demo/resources/main_page.js
+++ apps/rich_text_demo/resources/main_page.js
@@ -11,26 +11,37 @@ RichTextDemo.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView toolbarView editorView rawView cleanView'.w(),
+    childViews: 'labelView toolbarView editorLabel editorView rawLabel rawView cleanLabel cleanView reloadedLabel reloadedView'.w(),
 
     labelView: SC.LabelView.design({
-      layout: { top: 0, left: 0, width: 200, height: 18 },
+      layout: { top: 8, left: 0, width: 200, height: 25 },
       textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
+      controlSize: SC.LARGE_CONTROL_SIZE,
       value: "RichText Demo"
     }),
 
     toolbarView: RichText.ToolbarView.design({
-      layout: { top: 30, left: 0, width: 200, height: 300 },
+      layout: { top: 40, left: 15, width: 200, height: 325 },
       editorBinding: '.parentView.editorView'
     }),
 
-    editorView: RichText.EditorView.design({
-      layout: { top: 30, left: 225, width: 300, height: 300 }
+    editorLabel: SC.LabelView.design({
+      layout: { top: 15, left: 240, width: 300, height: 18 },
+      value: 'Editor'
     }),
 
-    rawView: SC.LabelView.design({
-      layout: { top: 30, left: 550, width: 300, height: 140 },
+    editorView: RichText.EditorView.design({
+      layout: { top: 40, left: 240, width: 300, height: 325 }
+    }),
+
+    rawLabel: SC.LabelView.design({
+      layout: { top: 15, left: 565, width: 300, height: 18 },
+      value: 'Raw HTML'
+    }),
+
+    rawView: SC.TextFieldView.design({
+      layout: { top: 40, left: 565, width: 300, height: 140 },
+      isTextArea: YES,
 
       _setValue: function(){
         var iframeHtml = this.getPath('parentView.editorView').$inputBody().html();
@@ -39,15 +50,37 @@ RichTextDemo.mainPage = SC.Page.design({
 
     }),
 
-    cleanView: SC.LabelView.design({
-      layout: { top: 190, left: 550, width: 300, height: 140 },
+    cleanLabel: SC.LabelView.design({
+      layout: { top: 200, left: 565, width: 300, height: 18 },
+      value: 'Cleaned HTML'
+    }),
+
+    cleanView: SC.TextFieldView.design({
+      layout: { top: 225, left: 565, width: 300, height: 140 },
+      isTextArea: YES,
 
       _setValue: function(){
         var value = this.getPath('parentView.editorView.value');
         this.set('value', value);
       }.observes('.parentView.editorView.value')
 
-    })
+    }),
+
+    reloadedLabel: SC.LabelView.design({
+      layout: { top: 15, left: 890, width: 300, height: 18 },
+      value: 'Reloaded HTML'
+    }),
+
+    reloadedView: SC.TextFieldView.design({
+      layout: { top: 40, left: 890, width: 300, height: 140 },
+      isTextArea: YES,
+
+      _setValue: function(){
+        var value = this.getPath('parentView.editorView.value');
+        this.set('value', RichText.HtmlSanitizer.formatHTMLInput(value));
+      }.observes('.parentView.editorView.value')
+
+    }),
   })
 
 });
