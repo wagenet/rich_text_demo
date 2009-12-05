5a200b857397ddbc786895c1dcc3ef332b281d6e
Peter Wagenet <peter.wagenet@gmail.com>
2009-12-05 11:52:35 -0500

    Updated Framework and added live preview toggle

--- apps/rich_text_demo/resources/main_page.js
+++ apps/rich_text_demo/resources/main_page.js
@@ -11,7 +11,11 @@ RichTextDemo.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView toolbarView editorLabel editorView rawLabel rawView cleanLabel cleanView reloadedLabel reloadedView'.w(),
+
+    previewEnabled: YES,
+
+    childViews: ('labelView toolbarView previewCheckbox previewLabel editorLabel editorView rawLabel rawView ' + 
+                 'cleanLabel cleanView reloadedLabel reloadedView').w(),
 
     labelView: SC.LabelView.design({
       layout: { top: 8, left: 0, width: 200, height: 25 },
@@ -21,8 +25,18 @@ RichTextDemo.mainPage = SC.Page.design({
     }),
 
     toolbarView: RichText.ToolbarView.design({
-      layout: { top: 40, left: 15, width: 200, height: 325 },
+      layout: { top: 40, left: 15, width: 200, height: 300 },
       editorBinding: '.parentView.editorView'
+    }),
+
+    previewCheckbox: SC.CheckboxView.design({
+      layout: { top: 350, left: 15 },
+      valueBinding: '.parentView.previewEnabled'
+    }),
+
+    previewLabel: SC.LabelView.design({
+      layout: { top: 350, left: 40, width: 175, height: 18 },
+      value: 'Live Preview (may be slow)'
     }),
 
     editorLabel: SC.LabelView.design({
@@ -43,9 +57,13 @@ RichTextDemo.mainPage = SC.Page.design({
       layout: { top: 40, left: 565, width: 300, height: 140 },
       isTextArea: YES,
 
+      isEnabledBinding: '.parentView.previewEnabled',
+
       _setValue: function(){
-        var iframeHtml = this.getPath('parentView.editorView').$inputBody().html();
-        this.set('value', iframeHtml);
+        if(this.get('isEnabled')) {
+          var iframeHtml = this.getPath('parentView.editorView').getFieldValue();
+          this.set('value', iframeHtml);
+        }
       }.observes('.parentView.editorView.value')
 
     }),
@@ -59,9 +77,13 @@ RichTextDemo.mainPage = SC.Page.design({
       layout: { top: 225, left: 565, width: 300, height: 140 },
       isTextArea: YES,
 
+      isEnabledBinding: '.parentView.previewEnabled',
+
       _setValue: function(){
-        var value = this.getPath('parentView.editorView.value');
-        this.set('value', value);
+        if(this.get('isEnabled')) {
+          var value = this.getPath('parentView.editorView.value');
+          this.set('value', value);
+        }
       }.observes('.parentView.editorView.value')
 
     }),
@@ -75,10 +97,11 @@ RichTextDemo.mainPage = SC.Page.design({
       layout: { top: 40, left: 890, width: 300, height: 140 },
       isTextArea: YES,
 
+      isEnabledBinding: '.parentView.previewEnabled',
+
       _setValue: function(){
-        var value = this.getPath('parentView.editorView.value');
-        this.set('value', RichText.HtmlSanitizer.formatHTMLInput(value));
-      }.observes('.parentView.editorView.value')
+        if(this.get('isEnabled')) this.set('value', this.getPath('parentView.editorView.fieldValue'));
+      }.observes('.parentView.editorView.fieldValue')
 
     }),
   })
